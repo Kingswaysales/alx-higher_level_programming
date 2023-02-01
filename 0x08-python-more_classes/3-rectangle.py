a6e8bf2dd88e912f35ed3dc8a2d1090b42125cbd
kingswaysales <marco.nkululeko@gmail.com>
2023-02-01 15:29:32 -0800

    task3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
 A module with a Rectangle that does nothing
+
+
 """
 
 
---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module is composed by a class that defines a Rectangle
+
+
+"""
+
+
+class Rectangle:
+    """ Class that defines a rectangle """
+
+
+    def __init__(self, width=0, height=0):
+        """ Method that initializes the instance
+
+        Args:
+            width: rectangle width
+            height: rectangle height
+
+        """
