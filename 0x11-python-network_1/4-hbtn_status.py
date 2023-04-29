6f7b2c2693a2f423135b2dfe464bb725a6fc2382
kingswaywalsales <marco.nkululeko@gmail.com>
2023-04-29 11:57:42 -0700

    task4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-    Use package requests to fetch https://intranet.hbtn.io/status
+    Use package requests to fetch https://alx-intranet.hbtn.io/status
 """
 import requests
 if __name__ == "__main__":
-    r = requests.get('https://intranet.hbtn.io/status').text
+    r = requests.get('https://alx-intranet.hbtn.io/status').text
     print("Body response:")
     print("\t- type: {}".format(type(r)))
     print("\t- content: {}".format(r))
