4afa243ea2dac0c1db7a4b27c72c85b034e0e554
kingswaywalsales <marco.nkululeko@gmail.com>
2023-05-23 13:45:19 -0700

    task4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,16 @@
 #!/usr/bin/node
-const request = require('request');
-const url = process.argv[2];
 
-request(url, (err, response, body) => {
-  if (err) {
-    console.log(err);
-  } else {
-    const apiData = JSON.parse(body);
-    const films = apiData.results;
-    const ID = 'https://swapi-api.hbtn.io/api/people/18/';
+let request = require('request');
+const url = process.argv[2];
+request(url, function (err, response, body) {
+  if (err) { console.log(err); }
+  if (response.statusCode === 200) {
+    let results = JSON.parse(body).results;
     let count = 0;
-    for (let i = 0; i < films.length; i++) {
-      const characters = films[i].characters;
-      for (let j = 0; j < characters.length; j++) {
-        if (characters[j] === ID) {
+    for (let res in results) {
+      let characters = results[res]['characters'];
+      for (let c in characters) {
+        if (characters[c].search('/18/') > 0) {
           count++;
         }
       }
