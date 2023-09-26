fcdfdd010720e997486c5287c0733e3ca31be578
kingswaysales <marco.nkululeko@gmail.com>
2023-09-26 12:36:10 -0700

    task5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,16 +1,15 @@
 #!/usr/bin/node
+
 const request = require('request');
-const url = process.argv[2];
 const fs = require('fs');
 
-request(url, (err, response, body) => {
-  if (err) {
-    console.log(err);
-  } else {
-    fs.writeFile(process.argv[3], body, 'utf-8', (err) => {
-      if (err) {
-        console.log(err);
-      }
-    });
-  }
-});
+request
+  .get(process.argv[2], function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      fs.writeFile(process.argv[3], body, 'utf8', function (err) {
+        if (err) { console.log(err); }
+      });
+    }
+  });
