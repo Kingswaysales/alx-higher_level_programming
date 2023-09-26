bba923c9563cfe7c75bfc02c8150912f6fa7d920
kingswaysales <marco.nkululeko@gmail.com>
2023-09-26 10:46:54 -0700

    delete tasks

--- 0x15-javascript-web_jquery/5-script.js
+++
@@ -1,4 +0,0 @@
-$('div#add_item').click(function () {
-  let item = $('<li></li>').text('Item');
-  $('ul.my_list').append(item);
-});
--- 0x15-javascript-web_jquery/7-script.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-  $.get('https://swapi.co/api/people/5/?format=json', function (data, status) {
-    if (status === 'success') {
-      $('div#character').text(data.name);
-    }
-  });
-});
